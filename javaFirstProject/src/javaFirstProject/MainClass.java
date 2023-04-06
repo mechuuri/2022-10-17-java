fc1648821011fa8afb01c8b21ae3d0b65b86e940
seo <116634427+mechuuri@users.noreply.github.com>
2023-04-06 20:41:27 +0900

    첫프

---
+++ javaFirstProject/src/javaFirstProject/MainClass.java
@@ -0,0 +1,12 @@
+package javaFirstProject;
+
+//control+F11
+public class MainClass {
+	// 한개 프로그램의 시작점
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java!!");
+		
+	}
+
+}
